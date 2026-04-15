508c2a9541016596a1f9e41aaead4e3243f0e1a1
lewismacharia1-coder <lewismacharia1@student.moringaschool.com>
2026-04-15 16:05:49 +0300

    done with the lab

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+const dayjs = require('dayjs');
 
+function combineUsers(...args) {
+    let combinedObject = {
+        users: []
+    };
+
+// This merges all the arrays passed in
+for (const userArray of args) {
+    combinedObject.users = [...combinedObject.users, ...userArray];
+}
+
+// This sets the date to today
+combinedObject.merge_date = dayjs().format('M/D/YYYY');
+
+return combinedObject;
+}
+
+module.exports = combineUsers;
 
 
 module.exports = {
